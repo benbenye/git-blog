18128bce48c44ebbed4ce1ad46df5653087a8e58
benbenye <1203846281@qq.com>
2018-04-06 17:33:42 +0800

    init

---
+++ precache-manifest.9a1d9dece257ce0a00b56ad7d35b0b3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b145bb17316fe38648b7",
+    "url": "/git-blog/js/vendor.b145bb17.js"
+  },
+  {
+    "revision": "9e97698b4e0e032c1912",
+    "url": "/git-blog/js/manifest.9e97698b.js"
+  },
+  {
+    "revision": "69e0ce3ec71f640db150",
+    "url": "/git-blog/js/app.69e0ce3e.js"
+  },
+  {
+    "revision": "6ac9625902c026d184b1a6a4616bdb29",
+    "url": "/git-blog/index.html"
+  },
+  {
+    "revision": "69e0ce3ec71f640db150",
+    "url": "/git-blog/css/app.8f2808e6.css"
+  }
+];
